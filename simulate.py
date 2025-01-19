f9099d1c08a06298aa1a5239f3d537fca0dcf6c5
jmontour15 <jmontour@uvm.edu>
2025-01-19 14:41:43 -0500

    Time mechanic

--- simulate.py
+++ simulate.py
@@ -1,1 +1,12 @@
 import pybullet as p
+import time
+
+physicsClient = p.connect(p.GUI)
+time_step = 1/30
+
+for i in range(1001):
+    print(i)
+    time.sleep(time_step)
+    p.stepSimulation()
+
+p.disconnect()
