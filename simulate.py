32a3ce0c808caf5881f494e84b41cd2264e2fef2
jmontour15 <jmontour@uvm.edu>
2025-01-19 19:39:06 -0500

    one link assignment

--- simulate.py
+++ simulate.py
@@ -1,9 +1,14 @@
+# Imports
 import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-time_step = 1/30
+time_step = 1/120 # sleeps 1/120 seconds per loop
 
+# Send box to simulation
+p.loadSDF("box.sdf")
+
+# Iterate 1000 times
 for i in range(1001):
     print(i)
     time.sleep(time_step)
